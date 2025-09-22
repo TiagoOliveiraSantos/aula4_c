69d4d90e818629a18d3b912a0c32efa8c0a2348a
Vanessa Santana <48105425+santanavanessa@users.noreply.github.com>
2025-09-22 23:45:29 +0000

    update script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function media() {
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+
+    if (!valor1 || !valor2) {
+        alert("Por favor, digite valores numéricos válidos em ambos os campos.");
+        return;
+    }
+
+    // Converte os valores para número e calcula a média
+    let resultado = (parseFloat(valor1) + parseFloat(valor2)) / 2;
+
+}
